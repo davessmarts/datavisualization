558af50437e085f2d253d0dfffd99b56045d2e2a
davessmarts <david.spektor88@gmail.com>
2014-04-15 11:37:33 -0400

    Create wmt-get-data.php

---
+++ wmt-get-data.php
@@ -0,0 +1,28 @@
+<?php
+    $username = "root"; 
+    $password = "happydavid1";   
+    $host = "localhost";
+    $database="test";
+
+    $server = mysql_connect($host, $user, $password);
+    $connection = mysql_select_db($database, $server);
+
+    $myquery = $_POST['query'];
+
+    $query = mysql_query($myquery);
+
+    if ( ! $myquery ) {
+        echo mysql_error();
+        die;
+    }
+
+    $data = array();
+
+    for ($x = 0; $x < mysql_num_rows($query); $x++) {
+        $data[] = mysql_fetch_assoc($query);
+    }
+
+    echo json_encode($data);     
+
+    mysql_close($server);
+?>
